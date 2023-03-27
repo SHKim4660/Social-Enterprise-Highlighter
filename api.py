3413302a55fed051f01a362ab8e4f81d5036e20b
hyc3573 <hyc3573@gmail.com>
2023-03-27 13:21:45 +0900

    renamed file

--- api.py
+++ api.py
@@ -2,9 +2,25 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/hello/', methods=['GET', 'POST'])
-def welcome():
-    return "Hello World!"
+@app.route('/userscript.user.js')
+def userscript():
+    str = ""
+    try:
+        with open("highlighter.user.js", "r") as f:
+            str = f.read()
+    except Exception as e:
+        print("Warning: Cannot read file.: ", e)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=105)
+    return str
+
+@app.route('/api/<string:vendor>')
+def api(vendor):
+    if vendor[0] == 'a':
+        return "YEP"
+    else:
+        return "NOP"
+    
+    
+
+if __name__ == "__main__":
+    app.run(port=8081)
