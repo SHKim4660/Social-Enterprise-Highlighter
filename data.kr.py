315a0cc9861d31a088e5f52142d74cbf9c3e2869
SHKim4660 <shkim4660@gmail.com>
2023-03-28 10:26:55 +0900

    Update data.kr.py

--- data.kr.py
+++ data.kr.py
@@ -4,10 +4,10 @@ from xml.etree.ElementTree import Element, dump, ElementTree
 import pprint
 
 page = 1
-url = (f"https://api.odcloud.kr/api/socialEnterpriseList/v1/authCompanyList?page{page}=&perPage=10&returnType=XML&serviceKey=U7TCyPP1H%2FdN%2FNSqmby2ep6u9Mp2IJ%2BymK4QhmZ%2FxkX7C4%2BIHA%2BCdHYHsGXEkIFvf%2FzYC4lwD1X02l0RC3d4nA%3D%3D")
+url = (f"https://api.odcloud.kr/api/socialEnterpriseList/v1/authCompanyList?page{page}=&perPage=10&serviceKey=U7TCyPP1H%2FdN%2FNSqmby2ep6u9Mp2IJ%2BymK4QhmZ%2FxkX7C4%2BIHA%2BCdHYHsGXEkIFvf%2FzYC4lwD1X02l0RC3d4nA%3D%3D")
 
 response = requests.get(url)
 data = response.text
 
-print(type(data))
+print(data)
 
