aae880644d68cbeaef98840fb999db98c61f6192
SHKim4660 <shkim4660@gmail.com>
2023-03-28 13:32:34 +0900

    Update data.kr.py

--- data.kr.py
+++ data.kr.py
@@ -1,13 +1,21 @@
 import requests
-import xml.etree.ElementTree as ET
-from xml.etree.ElementTree import Element, dump, ElementTree
+import json
 import pprint
 
-page = 1
-url = (f"https://api.odcloud.kr/api/socialEnterpriseList/v1/authCompanyList?page{page}=&perPage=10&serviceKey=U7TCyPP1H%2FdN%2FNSqmby2ep6u9Mp2IJ%2BymK4QhmZ%2FxkX7C4%2BIHA%2BCdHYHsGXEkIFvf%2FzYC4lwD1X02l0RC3d4nA%3D%3D")
+# page = 1
+# perpage = 10
+# url = (f"https://api.odcloud.kr/api/socialEnterpriseList/v1/authCompanyList?page={page}&perPage={perpage}&serviceKey=U7TCyPP1H%2FdN%2FNSqmby2ep6u9Mp2IJ%2BymK4QhmZ%2FxkX7C4%2BIHA%2BCdHYHsGXEkIFvf%2FzYC4lwD1X02l0RC3d4nA%3D%3D")
 
-response = requests.get(url)
-data = response.text
+def get_data(page,perpage):
+    page = page
+    perpage = perpage
+    url = (f"https://api.odcloud.kr/api/socialEnterpriseList/v1/authCompanyList?page={page}&perPage={perpage}&serviceKey=U7TCyPP1H%2FdN%2FNSqmby2ep6u9Mp2IJ%2BymK4QhmZ%2FxkX7C4%2BIHA%2BCdHYHsGXEkIFvf%2FzYC4lwD1X02l0RC3d4nA%3D%3D")
+    response = requests.get(url)
+    data = json.loads(response.text)
 
-print(data)
-
+    for i in range(perpage):
+        pro_data = data.get("data")[i].get("entNmV")
+        print(pro_data)
+    
+for i in range(10):
+    get_data(i,10)
