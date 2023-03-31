913f8782dbf963751608c471f882da9cbeb6b45f
SHKim4660 <shkim4660@gmail.com>
2023-03-31 10:53:42 +0900

    Update get_KRE100_data.py

--- get_KRE100_data.py
+++ get_KRE100_data.py
@@ -1,7 +1,7 @@
 import requests
 
 url = 'http://apis.data.go.kr/B553530/RENEWABLE/ENTE_LIST'
-params ={'serviceKey' : '서비스키', 'pageNo' : '1', 'numOfRows' : '10', 'apiType' : 'xml', 'q1' : 'kea공단', 'q2' : '2021', 'q3' : '2050' }
+params ={'serviceKey' : 'U7TCyPP1H/dN/NSqmby2ep6u9Mp2IJ+ymK4QhmZ/xkX7C4+IHA+CdHYHsGXEkIFvf/zYC4lwD1X02l0RC3d4nA==', 'pageNo' : '1', 'numOfRows' : '10', 'apiType' : 'json'}
 
 response = requests.get(url, params=params)
 print(response.content)
