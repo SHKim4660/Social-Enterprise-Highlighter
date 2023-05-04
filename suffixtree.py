eb18de9a4fb3bacfab3ac45b8c7342a5d2a8d1f0
hyc3573 <hyc3573@gmail.com>
2023-05-04 09:33:42 +0900

    suffix tree

---
+++ suffixtree.py
@@ -0,0 +1,20 @@
+import trie
+
+class SFXTree:
+    def __init__(self):
+        self.tr = trie.Trie()
+
+    def insert(self, string: str):
+        for suffix in [string[i:] for i in range(len(string))]:
+            self.tr.insert(suffix)
+
+    def search(self, query: str) -> bool:
+        return self.tr.search(query, match_prefix=True)
+
+if __name__ == "__main__":
+    st = SFXTree()
+
+    st.insert('abcdefg')
+
+    print(st.search("bc"))
+    print(st.search("ag"))
