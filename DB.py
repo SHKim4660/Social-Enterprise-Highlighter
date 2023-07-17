60d9e3e139a914c59514f17da706f02ef4397a26
SHKim4660 <shkim4660@gmail.com>
2023-07-17 12:33:51 +0900

    some

--- DB.py
+++ DB.py
@@ -9,18 +9,18 @@ def get_data(name,page,perpage):
     data_list = []
     pattern = r'\([^)]*\)'
     try:
-        if name == "사회적기업정보":
-            url = (f"https://api.odcloud.kr/api/socialEnterpriseList/v1/authCompanyList?page={page}&perPage={perpage}&serviceKey=U7TCyPP1H%2FdN%2FNSqmby2ep6u9Mp2IJ%2BymK4QhmZ%2FxkX7C4%2BIHA%2BCdHYHsGXEkIFvf%2FzYC4lwD1X02l0RC3d4nA%3D%3D")
-            api_response = requests.get(url)
-            api_data = json.loads(api_response.text)
-            for i in range(perpage):
-                 raw_data = api_data.get("data")[i].get("entNmV")  # 데이터를 받아와서 리스트 형태로 취합
-                 pro_data = re.sub(pattern=pattern, repl='', string= raw_data).replace("㈜","").replace("유)","").replace("주)","").replace("사)","").replace(" ","").replace("주식회사","").replace("사단법인","")
-                 pro_pro_data = f"{pro_data}0"
-                 data_list.append([pro_pro_data])
+        # if name == "사회적기업정보":
+        #     url = (f"https://api.odcloud.kr/api/socialEnterpriseList/v1/authCompanyList?page={page}&perPage={perpage}&serviceKey=U7TCyPP1H%2FdN%2FNSqmby2ep6u9Mp2IJ%2BymK4QhmZ%2FxkX7C4%2BIHA%2BCdHYHsGXEkIFvf%2FzYC4lwD1X02l0RC3d4nA%3D%3D")
+        #     api_response = requests.get(url)
+        #     api_data = json.loads(api_response.text)
+        #     for i in range(perpage):
+        #          raw_data = api_data.get("data")[i].get("entNmV")  # 데이터를 받아와서 리스트 형태로 취합
+        #          pro_data = re.sub(pattern=pattern, repl='', string= raw_data).replace("㈜","").replace("유)","").replace("주)","").replace("사)","").replace(" ","").replace("주식회사","").replace("사단법인","")
+        #          pro_pro_data = f"{pro_data}0"
+        #          data_list.append([pro_pro_data])
               
-            save_data(os.path.join('data', 'data.csv'),data_list) # 데이터를 csv파일 평태로 저장
-            print(data_list)
+        #     save_data(os.path.join('data', 'data.csv'),data_list) # 데이터를 csv파일 평태로 저장
+        #     print(data_list)
         
         if name == "K-RE100참여기업":
             url = (f"https://apis.data.go.kr/B553530/RENEWABLE/ENTE_LIST?serviceKey=U7TCyPP1H%2FdN%2FNSqmby2ep6u9Mp2IJ%2BymK4QhmZ%2FxkX7C4%2BIHA%2BCdHYHsGXEkIFvf%2FzYC4lwD1X02l0RC3d4nA%3D%3D&pageNo={page}&numOfRows={perpage}&apiType=json")
@@ -39,7 +39,7 @@ def get_data(name,page,perpage):
 
 #파일에 데이터 저장
 def save_data(filename,index):
-    file = open(filename,'a',newline='')
+    file = open(filename,'a',newline='',encoding = 'utf8')
     writer = csv.writer(file)
     writer.writerows(index)
 
--- api.py
+++ api.py
@@ -13,7 +13,7 @@ tr = trie.Trie()
 
 # 트라이 초기화 함수
 def trie_insert(filename):
-    file = open(filename,'r')
+    file = open(filename,'r',encoding = 'utf8')
     reader = csv.reader(file)
     for line in reader:
         tr.insert(line[0])
