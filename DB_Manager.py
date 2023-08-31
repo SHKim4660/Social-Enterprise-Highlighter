11c1aded00715801b09c0f1b8bc5340419a67df4
SHKim4660 <shkim4660@gmail.com>
2023-08-31 11:30:25 +0900

    data manager complete

--- DB_Manager.py
+++ DB_Manager.py
@@ -5,20 +5,20 @@ import pandas as pd
 
 # path == os.path.join('data', 'data.csv')
 
-file = os.path.join('data', 'data_test.csv')
+# test_filepath = os.path.join('data', 'data_test.csv')
 
 def remove_data(filepath,data):
-    filepath,data
+    with open(filepath, 'rt', encoding='UTF8') as file:
+        lines = file.readlines()
+    with open(filepath, "w") as file:
+        for line in lines:
+            if line.strip("\n") != data:     # <= 이 문자열만 골라서 삭제
+                file.write(line)
 
-# target_rows = []
-# for row_index, row in file.iterrows():    
-#     if row.loc[0] == "가나다":
-#         target_rows.append(row_index)
+def add_data(filepath,data):
+    with open(filepath, "a") as file:
+        file.write(f"\n{data}\n")
 
-with open(file, 'rt', encoding='UTF8'):
-    lines = file.readlines()
-with open(file, "w"):
-    for line in lines:
-        if line.strip("\n") != "가나다":     # <= 이 문자열만 골라서 삭제
-            file.write(line)
 
+# remove_data(filepath,"가나다")
+# add_data(test_filepath,"가라다")
